be6c81fdba1defe6651985cb0cc21b279712fba5
MohammedJabbar <91497295+MohammedJabbar@users.noreply.github.com>
2023-01-20 12:22:41 +0300

    Add files via upload

---
+++ Memory.py
@@ -0,0 +1,111 @@
+import psutil
+import time
+import os
+import GPUtil
+from tabulate import tabulate
+
+def get_size(bytes, suffix="B"):
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
+
+def progress_bar(current, total, bar_length=20):
+    fraction = current / total
+
+    arrow = int(fraction * bar_length - 1) * '>' + '>'
+    padding = int(bar_length - len(arrow)) * ' '
+
+    ending = '\n' if current == total else '\n'
+
+    print(f' : [{arrow}{padding}] {current}%', end=ending)
+# Memory Information
+while 1:
+    # Memory Information
+    print("="*20, "Memory Information", "="*20)
+    # get the memory details
+    svmem = psutil.virtual_memory()
+    print(f"Total: {get_size(svmem.total)}")
+    print(f"Available: {get_size(svmem.available)}")
+    print(f"Used: {get_size(svmem.used)}")
+    print(f"Percentage: {svmem.percent}%" ,end=" ")
+    progress_bar(svmem.percent,100)
+    print("="*20, "SWAP", "="*20)
+    # get the swap memory details (if exists)
+    swap = psutil.swap_memory()
+    print(f"Total: {get_size(swap.total)}")
+    print(f"Free: {get_size(swap.free)}")
+    print(f"Used: {get_size(swap.used)}")
+    print(f"Percentage: {swap.percent}%" ,end=" ")
+    progress_bar(swap.percent,100)
+    # let's print CPU information
+    print("="*20, "CPU Info", "="*20)
+    # number of cores
+    print("Physical cores:", psutil.cpu_count(logical=False))
+    print("Total cores:", psutil.cpu_count(logical=True))
+    # CPU frequencies
+    cpufreq = psutil.cpu_freq()
+    print(f"Max Frequency: {cpufreq.max}Mhz")
+    print(f"Min Frequency: {cpufreq.min}Mhz")
+    print(f"Current Frequency: {cpufreq.current}Mhz")
+    # CPU usage
+    print("CPU Usage Per Core:")
+    #for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
+      #  print(f"Core {i}: {percentage}%")
+    Usage = psutil.cpu_percent()
+    print(f"Total CPU Usage: {Usage}%",end=" ")
+    progress_bar(Usage,100)
+    # Disk Information
+    print("="*20, "Disk Information", "="*20)
+    print("Partitions and Usage:")
+    # get all disk partitions
+    partitions = psutil.disk_partitions()
+    for partition in partitions:
+        print(f"=== Device: {partition.device} ===")
+        print(f"  Mountpoint: {partition.mountpoint}")
+        print(f"  File system type: {partition.fstype}")
+        try:
+            partition_usage = psutil.disk_usage(partition.mountpoint)
+        except PermissionError:
+            # this can be catched due to the disk that
+            # isn't ready
+            continue
+        print(f"  Total Size: {get_size(partition_usage.total)}")
+        print(f"  Used: {get_size(partition_usage.used)}")
+        print(f"  Free: {get_size(partition_usage.free)}")
+        print(f"  Percentage: {partition_usage.percent}%",end=" ")
+        progress_bar(partition_usage.percent,100)
+        # get IO statistics since boot
+    disk_io = psutil.disk_io_counters()
+    print(f"Total read: {get_size(disk_io.read_bytes)}")
+    print(f"Total write: {get_size(disk_io.write_bytes)}")
+
+    print("="*40, "GPU Details", "="*40)
+    gpus = GPUtil.getGPUs()
+    list_gpus = []
+    for gpu in gpus:
+        # get the GPU id
+        gpu_id = gpu.id
+        # name of GPU
+        gpu_name = gpu.name
+        # get % percentage of GPU usage of that GPU
+        gpu_load = f"{gpu.load*100}%"
+        # get free memory in MB format
+        gpu_free_memory = f"{gpu.memoryFree}MB"
+        # get used memory
+        gpu_used_memory = f"{gpu.memoryUsed}MB"
+        # get total memory
+        gpu_total_memory = f"{gpu.memoryTotal}MB"
+        # get GPU temperature in Celsius
+        gpu_temperature = f"{gpu.temperature} °C"
+        gpu_uuid = gpu.uuid
+        list_gpus.append((
+            gpu_id, gpu_name, gpu_load, gpu_free_memory, gpu_used_memory,
+            gpu_total_memory, gpu_temperature, gpu_uuid
+        ))
+
+    print(tabulate(list_gpus, headers=("id", "name", "load", "free memory", "used memory", "total memory",
+                                       "temperature", "uuid")))
+    time.sleep(1)
+    os.system('cls')
